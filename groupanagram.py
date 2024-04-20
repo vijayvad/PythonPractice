4216e8925a9d2a83f18a2d9f517d98a9e2907ce3
vijayvad <vijayvad@gmail.com>
2024-04-20 10:55:26 -0400

    Create groupanagram.py

---
+++ groupanagram.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+        result=[]
+        if len(strs) ==0: return [[""]]
+        if len(strs) ==1: return [strs]
+        for s in strs:
+            temp = ''.join(sorted(s))
+            print(temp)
+            print(s)
+            if temp in dict1:
+                x = dict1[temp]
+                x.append(s)
+                dict1[temp] = x
+            else:
+                dict1[temp] = [s]
+        for i,v in dict1.items():
+            result.append(v)
+        return result
+
+c=Solution()
+print(c.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
